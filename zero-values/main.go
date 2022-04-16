3693e7466880d280a8c56192e51475ea3bcfdbd7
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-04-16 18:48:48 -0300

    Valor padrão dos tipos de dados

---
+++ zero-values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 4
+	var b = 5.2
+
+	a = int(b)
+	fmt.Println(a, b)
+
+	var value int
+	var prince float64
+	var name string
+	var done bool
+	fmt.Println(value, prince, name, done)
+}
